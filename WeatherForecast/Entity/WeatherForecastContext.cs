be820f4458fd1083db06f97b0c402967e77ed941
Nangcr <nangcr@gmail.com>
2021-07-19 16:39:24 +0800

    添加了数据库实体

---
+++ WeatherForecast/Entity/WeatherForecastContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherForecast.Entity
+
+{
+    public class WeatherForecastContext : DbContext
+    {
+        public WeatherForecastContext(DbContextOptions<WeatherForecastContext> options) : base(options) { }
+
+        public DbSet<StudentEntity> Student { get; set; }
+    }
+}
